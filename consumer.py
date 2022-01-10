5b188e9a308d875a89117d2b929f60efd4af3c91
rusgarifull <rusgarifull@gmail.com>
2022-01-10 23:01:47 +0100

    refactor code

--- consumer.py
+++ consumer.py
@@ -1,26 +1,30 @@
 from time import sleep
 from json import loads
 from kafka import KafkaConsumer
-from mysql_connector import insert_into_test_table
+from mysql_connector import insert_into_table, create_table
 
 def main():
-    # set up a consumer
+    # source topic
     topic = 'second_topic'
+    # taget MySQL db/table
+    database = "kafka_messages"
+    table = "test_table"
+
+    # set up a Kafka consumer
     consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'],
                             auto_offset_reset='earliest', enable_auto_commit=True, 
-                            group_id='my-first-app',
-                            value_deserializer=lambda x: loads(x.decode('utf-8')))
+                            group_id='my-first-app', value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    # create table (if not exists) to store messages 
+    create_table(database, table)
 
     # read messages from the topic
     for message in consumer:
-        try:
-            # Read messages from the topic and write to mysql table
-            message = message.value
-            print("Message recieved: {}. Writing to MySQL".format(message))
-            insert_into_test_table((message,))
-            #print('{} added to {}'.format(message, collection))
+        message = message.value
+        print("Message recieved: {}. Writing to MySQL".format(message))
+        
+        # write to mysql table
+        insert_into_table(database, table, (message,))
 
-        except Exception as e:
-            print("Error: ", e)
 if __name__ == '__main__':
     main()
--- mysql_connector.py
+++ mysql_connector.py
@@ -1,36 +1,61 @@
 from getpass import getpass
 from mysql.connector import connect, Error
+import re
 
 host = "localhost"
 user = "root"
 password = getpass("Enter password: ")
 
-def create_test_table():
+def anti_injections_check(name):
+    """
+    Checks if a string is a proper table/database name. 
+    It helps to prevent SQL injections. 
+    Returns True if a string is a proper name. Otherwise raises an error
+    """
+    name_validator = re.compile(r'^[0-9a-zA-Z_\$]+$')
+    if not name_validator.match(name):
+        raise ValueError('Hey!  No SQL injecting allowed!')
+    else:
+        return True
+
+
+def create_table(db, table):
+    """
+    Creates a table in a specified database
+    """
     try:
         with connect(host=host, user=user,password=password) as connection:
-            create_db_query = "CREATE DATABASE IF NOT EXISTS kafka_messages"
+            create_db_query = "CREATE DATABASE IF NOT EXISTS {}".format(db)
             create_table_query = """
-            CREATE TABLE IF NOT EXISTS kafka_messages.test_table (
+            CREATE TABLE IF NOT EXISTS {}.{} (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 message VARCHAR(100)
             )
-            """
-            with connection.cursor() as cursor:
-                cursor.execute(create_db_query)
-                cursor.execute(create_table_query)
+            """.format(db,table)
+
+            # check for potentials SQL injections and create a table 
+            if anti_injections_check(table):
+                with connection.cursor() as cursor:
+                    cursor.execute(create_db_query)
+                    cursor.execute(create_table_query)
 
     except Error as e:
         print("Error: ", e)
 
 
-def insert_into_test_table(message_data):
+def insert_into_table(db, table, data):
+    """
+    Inserts data to a table
+    """
     try:
-        with connect(host=host, user=user,password=password, database="kafka_messages") as connection:
-            insert_query = "INSERT INTO test_table (message) VALUES (%s)"
-            with connection.cursor() as cursor:
-                cursor.execute(insert_query,message_data)
-                connection.commit()
-                print("message {} inserted".format(message_data))
+        with connect(host=host, user=user, password=password, database=db) as connection:
+            # check for potentials SQL injections and create a table 
+            if anti_injections_check(table):
+                insert_query = "INSERT INTO {} (message) VALUES (%s)".format(table)
+                with connection.cursor() as cursor:
+                    cursor.execute(insert_query,data)
+                    connection.commit()
+                    print("Message {} inserted".format(data))
     except Error as e:
         print("Error: ", e)
-
+        
